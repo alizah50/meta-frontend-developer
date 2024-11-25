326f163c8535ab5c13becb410d6d788f8562910e
aliza50 <shaukat_a@hotmail.com>
2024-11-24 23:49:23 -0500

    updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,10 +6,12 @@ const Banner = () => {
   return (
     <section className='Banner'>
       {/* Banner text */}
+      <img src={bannerImg} alt="Banner Img" className='banner-img-mobile' />
       <div>
+     
         <h2>Little Lemon</h2>
         <h3>Chicago</h3>
-        <img src={bannerImg} alt="Banner Img" className='banner-img-mobile' />
+       
         <p>We are a family owned Mediterranean restaurant, <br className='mobile-hidden'/>focused on traditional recipes served with a modern twist.</p>
         <Link to="/booking">
           <button aria-label='On Click'>Reserve a table</button>
