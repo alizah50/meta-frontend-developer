1134db4e45acbfd7ba77f01d4b22592a25429dad
aliza50 <shaukat_a@hotmail.com>
2024-11-24 23:38:20 -0500

    Updated with components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
 import './App.css';
-import logo from '../images/logo .svg'
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Menu from './components/Menu';
 
 function App() {
   return (
- 
-   <>
-   
-   </>
+    <>
+      <Header/>
+      <Main/>
+      <Menu/>
+      <Footer/>
+    </>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from '../images/logo .svg'
+import React, { useState } from 'react'
 
-const Nav = ()  => { 
-    
-  return ( 
-   <nav>
-   <a href='/'>
-   <img src={logo} alt='logo'/>
-   </a>
-  {/* mobile navbar */}
 
-  <div>
-  <div classname='bar'></div>
-  <div classname='bar'></div>
-  <div classname='bar'></div>
+const Nav = () => {
 
-  {/* nav items */}
-  </div>
-  
-  
-   </nav>
-  );
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
+  return (
+    <nav className='navbar'>
+      {/* Mobile Nav hamburger */}
+      <div className='menu-icon' onClick={toggleMenu}>
+        <div className='hamburger-bar'></div>
+        <div className='hamburger-bar'></div>
+        <div className='hamburger-bar'></div>
+      </div>
+
+      {/* Nav items */}
+      <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Services</a></li>
+        <li><a href="/">Menu</a></li>
+        <li><a href="/">Reservations</a></li>
+        <li><a href="/">Order Online</a></li>
+        <li><a href="/">Login</a></li>
+      </ul>
+    </nav>
+  )
 }
 
-export default Nav;
+export default Nav
